5e62d602247a9773c4851e37ee8165298e0fa774
Serge <sergelab@gmail.com>
2017-05-14 23:35:39 +0300

    карточка персоны (добавление/редактирование) — пока только ФИО

--- src/persons/admin.py
+++ src/persons/admin.py
@@ -5,7 +5,7 @@ from admin.views import admin
 from flask import current_app, flash, render_template, request, redirect, url_for
 from flask_babel import gettext as _
 from flask_login import login_required
-from yustina.forms.persons import PositionForm
+from yustina.forms.persons import PersonForm, PositionForm
 from yustina.init import db
 from yustina.models.persons import Person, Position
 
@@ -64,10 +64,10 @@ def persons_manage_position(position_id=None):
                     return redirect(url_for('admin.persons_manage_position',
                                             position_id=position_id))
 
-    if form.validate_on_submit():
         if position_id and position.in_trash is True:
             return redirect(url_for('admin.persons_positions'))
 
+    if form.validate_on_submit():
         try:
             form.populate_obj(position)
             db.session.add(position)
@@ -104,4 +104,67 @@ def persons_persons():
 @admin.route('/persons/<int:person_id>', methods=['GET', 'POST'])
 @login_required
 def persons_manage_person(person_id=None):
-    pass
+    if person_id:
+        query = Person.query.filter(Person.id.__eq__(person_id))
+        person = query.first()
+
+        if not person:
+            flash(_('Person not found message'), 'warning')
+            return redirect(url_for('admin.persons_persons'))
+    else:
+        person = Person()
+
+    form = PersonForm(obj=person)
+    keep_location = 'save_and_continue' in request.form
+
+    if request.method == 'POST':
+        if person_id:
+            if 'delete' in request.form:
+                try:
+                    person.delete()
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()
+                    current_app.logger.exception(e)
+                    flash(_('Error then delete object message'), 'danger')
+                else:
+                    flash(_('Delete object successfully message'), 'success')
+
+                    return redirect(url_for('admin.persons_persons'))
+            elif 'restore' in request.form:
+                try:
+                    person.restore()
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()
+                    current_app.logger.exception(e)
+                    flash(_('Error then restore object message'), 'danger')
+                else:
+                    flash(_('Restore object successfully message'), 'success')
+                    return redirect(url_for('admin.persons_manage_person',
+                                            person_id=person_id))
+
+        if person_id and person.in_trash is True:
+            return redirect(url_for('admin.persons_persons'))
+
+    if form.validate_on_submit():
+        try:
+            form.populate_obj(person)
+            db.session.add(person)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.exception(e)
+            flash(_('Error then add person message') if not person_id else
+                  _('Error then modify person message'), 'danger')
+        else:
+            flash(_('Person added successfully message') if not person_id else
+                  _('Person saved successfully message'), 'success')
+
+            return redirect(url_for('admin.persons_persons') if not keep_location else
+                            url_for('admin.persons_manage_person', person_id=person.id))
+
+    return render_template('admin/persons/manage_person.j2',
+                           person=person,
+                           person_id=person_id,
+                           form=form)
--- src/yustina/forms/persons.py
+++ src/yustina/forms/persons.py
@@ -9,6 +9,8 @@ from wtforms.widgets import TextInput
 
 
 class PositionForm(Form):
+    """ Должность
+    """
     name = StringField(
         __('Position name label'),
         validators=[
@@ -18,5 +20,34 @@ class PositionForm(Form):
             TextInput(),
             class_='uk-width-1-1',
             autofocus=True
+        )
+    )
+
+
+class PersonForm(Form):
+    """ Карточка персоны
+    """
+    surname = StringField(
+        __('Person surname label'),
+        validators=[DataRequired()],
+        widget=WidgetPrebind(
+            TextInput(),
+            class_='uk-width-1-2',
+            autofocus=True
+        )
+    )
+    firstname = StringField(
+        __('Person firstname label'),
+        validators=[DataRequired()],
+        widget=WidgetPrebind(
+            TextInput(),
+            class_='uk-width-1-2'
+        )
+    )
+    middlename = StringField(
+        __('Person middlename label'),
+        widget=WidgetPrebind(
+            TextInput(),
+            class_='uk-width-1-2'
         )
     )
