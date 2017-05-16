81180534201e6c0a0b2c6846c7d845232d3b7f27
Serge <sergelab@gmail.com>
2017-05-17 00:46:15 +0300

    зарегистрировали шаблонный фильтр date

--- src/yustina/views.py
+++ src/yustina/views.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from __future__ import absolute_import
+
+from contrib.utils.datetime import date_filter
 from flask import current_app, send_from_directory
+
 from .init import app
 
 
@@ -19,3 +22,8 @@ def assets(filename):
     """
     path = current_app.config.get('WEBPACK_ROOT')
     return send_from_directory(path, filename)
+
+
+@app.template_filter()
+def date(_date):
+    return date_filter(_date)
