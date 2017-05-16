3ec5bff6a3b197ca90738bf11e4f7878067811d1
Serge <sergelab@gmail.com>
2017-05-17 00:47:33 +0300

    модель для новостей

---
+++ src/yustina/models/press.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+from .mixins import DeletableMixin
+from ..init import db
+
+
+class NewsArticle(db.Model, DeletableMixin):
+    """
+    Новость.
+    """
+    __tablename__ = 'news'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    subtitle = db.Column(db.Text)
+    date_publishing = db.Column(db.Date, nullable=False)
+    annotation = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    source = db.Column(db.Text)
+    source_link = db.Column(db.Text)
+
+    @classmethod
+    def admin_list(cls):
+        query = cls.query
+        query = query.order_by(cls.date_publishing.desc())
+        return query
