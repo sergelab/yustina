98fa0020712f450dc7d55651483cfd337c3f9d8c
Serge <sergelab@gmail.com>
2017-06-29 22:56:07 +0300

    публичные представления для персон

--- src/persons/views.py
+++ src/persons/views.py
@@ -1,8 +1,37 @@
 # coding: utf-8
 from __future__ import absolute_import
 
+from flask import (abort,
+                   Blueprint,
+                   render_template)
 from persons.admin import *
-from flask import Blueprint
+from yustina.models.persons import Person
 
 
 bp = Blueprint('persons', __name__, template_folder='templates', url_prefix='/persons')
+
+
+@bp.route('/')
+def persons():
+    persons = Person.available_list().all()
+
+    return render_template('persons_list.j2',
+                           persons=persons)
+
+
+@bp.route('/<path:slug>')
+def person_card(slug):
+    person_query = Person.available_list(with_positions=True)
+    person_query = person_query.filter(
+        Person.slug == slug
+    )
+    person = person_query.first()
+
+    if not person:
+        abort(404)
+
+    workcases = person.available_workcases().all()
+
+    return render_template('person_card.j2',
+                           person=person,
+                           workcases=workcases)
