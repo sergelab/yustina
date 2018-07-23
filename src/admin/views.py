ae2608f6e8280641a0771af7e435293e45e9635c
Serge <sergelab@gmail.com>
2018-07-23 10:58:18 +0300

    nav management

--- src/admin/views.py
+++ src/admin/views.py
@@ -3,14 +3,22 @@ from __future__ import absolute_import
 
 from auth.models import User
 
-from flask import Blueprint, current_app, flash, g, redirect, render_template,\
-    request, url_for
-
+from flask import (abort,
+                   Blueprint,
+                   current_app,
+                   flash,
+                   g,
+                   redirect,
+                   render_template,
+                   request,
+                   url_for)
 from flask_babel import gettext as _, lazy_gettext as __
 from flask_login import current_user, login_required, login_user, logout_user
+from yustina.forms.navigation import NavigationOptionForm
 from yustina.forms.settings import SettingsForm
 from yustina.forms.users import LoginForm
 from yustina.init import app, db, lm
+from yustina.models.navigation import Navigation
 from yustina.models.settings import Settings, SettingsHelper
 
 
@@ -98,7 +106,10 @@ def admin_before_request():
              ]),
         dict(title=_('Settings nav option'),
              desciption=_('Settings dashboard description'),
-             view='admin.settings')
+             view='admin.settings'),
+        dict(title=_('Navigation nav option'),
+             desciption=_('Navigation dashboard description'),
+             view='admin.navigation')
     ]
 
 
@@ -141,6 +152,7 @@ def login():
 
 
 @admin.route('/settings', methods=['GET', 'POST'])
+@login_required
 def settings():
     settings = Settings.query.all()
     helper = SettingsHelper(settings)
@@ -160,4 +172,102 @@ def settings():
             return redirect(url_for('admin.settings'))
 
     return render_template('admin/settings/settings.j2',
+                           form=form)
+
+
+@admin.route('/navigation', methods=['GET', 'POST'])
+@login_required
+def navigation():
+    nav_options = Navigation.admin_list().all()
+
+    if request.method == 'POST':
+        if 'save_order' in request.form:
+            priorities = request.form.getlist('priority', type=int)
+            pos_nav_options = {o.id: o for o in nav_options}
+
+            for idx, priority in enumerate(priorities):
+                obj = pos_nav_options.get(priority)
+                if obj:
+                    obj.priority = idx + 1
+
+            try:
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                current_app.logger.exception(e)
+                flash(_("Error the save order message"), 'danger')
+            else:
+                flash(_('Order saved successfully message'), 'success')
+                return redirect(url_for('admin.navigation'))
+        else:
+            abort(405)
+
+    return render_template('admin/navigation/navigation.j2',
+                           nav_options=nav_options)
+
+
+@admin.route('/navigation/add', methods=['GET', 'POST'])
+@admin.route('/navigation/<int:option_id>', methods=['GET', 'POST'])
+@login_required
+def manage_navigation_option(option_id=None):
+    if option_id:
+        option = Navigation.query.filter(Navigation.id == option_id).one_or_none()
+        if not option:
+            flash(_('Navigation option not found message'), 'warning')
+            return redirect(url_for('admin.navigation'))
+    else:
+        option = Navigation()
+
+    form = NavigationOptionForm(obj=option)
+
+    if request.method == 'POST':
+        if option_id:
+            if 'delete' in request.form:
+                try:
+                    option.delete()
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()
+                    current_app.logger.exception(e)
+                    flash(_('Error then delete object message'), 'danger')
+                else:
+                    flash(_('Delete object successfully message'), 'success')
+
+                    return redirect(url_for('admin.navigation'))
+            elif 'restore' in request.form:
+                try:
+                    option.restore()
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()
+                    current_app.logger.exception(e)
+                    flash(_('Error then restore object message'), 'danger')
+                else:
+                    flash(_('Restore object successfully message'), 'success')
+                    return redirect(url_for('admin.manage_navigation_option',
+                                            option_id=option_id))
+
+        if option_id and option.in_trash is True:
+            return redirect(url_for('admin.navigation'))
+
+    if form.validate_on_submit():
+        form.populate_obj(option)
+
+        try:
+            db.session.add(option)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.exception(e)
+            flash(_("Can't save navigation option message"), 'danger')
+        else:
+            flash(_('Navigation option saved successfully message'), 'success')
+
+            return redirect(url_for('admin.manage_navigation_option',
+                                    option_id=option.id) if 'save_and_continue' in request.form
+                            else url_for('admin.navigation'))
+
+    return render_template('admin/navigation/manage_option.j2',
+                           option_id=option_id,
+                           option=option,
                            form=form)
