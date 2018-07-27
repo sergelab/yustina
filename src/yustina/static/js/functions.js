b57b42d29d01e4afae1e9cb9634d6d296a0b8336
Serge <serge@pgtech.ru>
2018-07-27 15:08:31 +0300

    upd: javascripts

--- src/yustina/static/js/functions.js
+++ src/yustina/static/js/functions.js
@@ -1,11 +1,10 @@
 $(function(){
-	$('video').click(function(){
-		$(this).remove();
-	});
 	$('.bio-link a').click(function(){
-		$('#bio').show(300);
-		$('body').animate({ scrollTop: $('#bio').offset().top }, 300);
+		$('#bio').slideDown(300);
 		return false;
+	});
+	$('#bio').click(function(){
+		$(this).slideUp(300);
 	});
 
 	var h = 0;
@@ -19,28 +18,37 @@ $(function(){
 	var slideNow = 1;
 	var slideCount = $('#viewport ul').children().length;
 	var translateWidth = 0;
+	var autoSlide = true;
 
-	function nextSlide(){
-		if(slideNow >= slideCount || slideNow <= 0){
-			$('#viewport ul').css('transform', 'translate(0,0)');
-			slideNow = 1;
+	function nextSlide(n = false){
+		if(n === false){
+			if(slideNow >= slideCount || slideNow <= 0){
+				$('#viewport ul').css('transform', 'translate(0,0)');
+				slideNow = 1;
+			} else {
+				translateWidth = -$('#viewport').width() * slideNow;
+				$('#viewport ul').css('transform', 'translate(' + translateWidth + 'px, 0)');
+				slideNow++;
+			}
 		} else {
-			translateWidth = -$('#viewport').width() * slideNow;
-			$('#viewport ul').css('transform', 'translate(' + translateWidth + 'px, 0)');
-			slideNow++;
+			slideNow = n;
+			nextSlide();
 		}
+		$('#circles > div').removeClass('current');
+		$('#circles > div:nth-child(' + slideNow + ')').addClass('current');
 	}
 
 	var timer = setInterval(nextSlide, 5000);
-	$('#viewport').hover(function(){ clearInterval(timer); }, function(){ timer = setInterval(nextSlide, 5000); });
+	$('#viewport').hover(function(){ clearInterval(timer); }, function(){ if(autoSlide) timer = setInterval(nextSlide, 5000); });
 
+	$('#circles > div').click(function(){
+		clearInterval(timer);
+		autoSlide = false;
+		nextSlide(parseInt($(this).attr('data-slide')));
+	});
 
 	$('.tabs > nav > ul > label').on('click', function(e){
 		$('.tabs > nav > ul > label').removeClass('active');
 		$(this).addClass('active');
-	});
-
-	$('#bio').click(function(){
-		$(this).hide(300);
 	});
 });
