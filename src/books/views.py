e2498b4b77ebb022e94d76326c1ae5b3cc03894b
Serge <serge@pgtech.ru>
2018-07-30 20:01:58 +0300

    books initial

--- src/books/views.py
+++ src/books/views.py
@@ -14,10 +14,27 @@ bp = Blueprint('books', __name__, template_folder='templates', url_prefix='/book
 
 @bp.route('/')
 def books():
-    books_list = Book.available_list().all()
+    books_list = Book.available_list().limit(
+        current_app.config.get('BOOKS_LOADING_LIMIT')).all()
 
     return render_template('books.j2',
                            books=books_list)
+
+
+@bp.route('/a/loading')
+def loading():
+    skip = request.args.get('skip', type=int) or None
+    data = []
+
+    if skip:
+        books_list = Book.available_list().offset(skip).limit(
+            current_app.config.get('BOOKS_LOADING_LIMIT')).all()
+        if books_list:
+            for b in books_list:
+                data.append(render_template('book_row.j2',
+                                            book=b))
+
+    return u'\n'.join(data)
 
 
 @bp.route('/<path:slug>')
